8b6d2101a176a8a645ed42acae53203d61d1cf67
alijavaboy <ali77erfagh99@gmail.com>
2022-04-15 18:42:57 +0430

    all test pass

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -5,33 +5,40 @@ public class StringCalculator {
     public int add(String numbers) {
         if (numbers.isEmpty()) return 0;
         else
-            return convertToArray(numbers);
+            return sumNumbers(numbers);
     }
 
-    public int convertToArray(String text)
+    public int sumNumbers(String string)
     {
-        int sum=0;
-        String[] split = text.split("");
-        for (String s : split)
-        {
-            if (validation(s))
-            {
-                sum=sum+sumNumbers(s);
-            }
-        }
-        return sum;
-    }
-
-    public int sumNumbers(String number) {
         int sum = 0;
-        int digit = convertToInteger(number);
-        return sum + digit;
-    }
+        String[] array = convertToArray(string);
+        String sumNumber = "";
+        for (String s : array)
+        {
+            if (validation(s)) {
+                sumNumber = sumNumber + s;
+                if (sumNumber.length() > 3) {
+                    sumNumber = "";
+                }
+            }
+             else {
+                 if (!sumNumber.isEmpty()&&!sumNumber.isBlank()) {
+                     sum += convertToInteger(sumNumber);
+                 }
+                sumNumber = "";
+            }
 
+        }
+       return sum+convertToInteger(sumNumber);
+
+    }
+    private String [] convertToArray(String array)
+    {
+       return array.split("");
+    }
     private int convertToInteger(String number) {
         return Integer.parseInt(number);
     }
-
     private boolean validation(String value) {
         String regex = "[0-9]+";
         return Pattern.matches(regex, value);
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -30,12 +30,51 @@ class StringCalculatorTest {
 
     @Test
     public void twoNumber() {
-
         int add = calculator.add("1,2");
         assertEquals(3, add);
         int add1 = calculator.add("2,2");
         assertEquals(4, add1);
         int add2 = calculator.add("5,2");
         assertEquals(7, add2);
+    }
+
+    @Test
+    public void enterNumbers() {
+        int add = calculator.add("1,2,3,4");
+        assertEquals(10, add);
+        int add1 = calculator.add("2,2,2,2,2,2,2,2");
+        assertEquals(16, add1);
+        int add2 = calculator.add("5,2,7");
+        assertEquals(14, add2);
+    }
+
+    @Test
+    public void enterNumberWhitCharacter() {
+        int add = calculator.add("1\n2,3");
+        assertEquals(6, add);
+        int add1 = calculator.add("//;\n1;2");
+        assertEquals(3, add1);
+        int add2 = calculator.add("//[*][%]\n1*2%3");
+        assertEquals(6, add2);
+        int add3 = calculator.add("//[***]\n1***2***3");
+        assertEquals(6, add3);
+        int add4 = calculator.add("//[foo][bar]\n1foo2bar3");
+        assertEquals(6, add4);
+    }
+
+    @Test
+    public void setCalculatorTest() {
+        int add = calculator.add("2,22");
+        assertEquals(24, add);
+        int add1 = calculator.add("22,22");
+        assertEquals(44, add1);
+        int add2 = calculator.add("2,11,2");
+        assertEquals(15, add2);
+    }
+
+    @Test
+    public void NumberBiggerThan1000() {
+        int add = calculator.add("1001,2");
+        assertEquals(2, add);
     }
 }
