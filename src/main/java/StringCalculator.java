491d7f2250344382ec561f313ede2f5849a42f95
alijavaboy <ali77erfagh99@gmail.com>
2022-04-17 23:20:39 +0430

    test  //[1_€#@546] 1*1_€#@546*2*1_€#@546*3 //[foo][bar]\n1foo2bar3 pass

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -5,10 +5,10 @@ public class StringCalculator {
     public int add(String numbers) {
         if (numbers.isEmpty()) return 0;
         else
-            return sumNumbers(numbers);
+            return sumNumbers(textCorrection(numbers));
     }
 
-    public int sumNumbers(String string) {
+    private int sumNumbers(String string) {
         int sum = 0;
         String[] array = convertToArray(string);
         String sumNumber = "";
@@ -16,10 +16,7 @@ public class StringCalculator {
         for (int index = 0; index < array.length; index++) {
             if (validation(array[index])) {
                 if (index > 1) {
-                    if (array[index].equals("1") &&
-                            array[index].equals("1") && ( validationStar(array[index-1]) && validationStar(array[index+1])) &&
-                            (!array[index - 1].equals(",") &&
-                                    !array[index + 1].equals(",")))
+                    if (isTrue(array, index))
                         array[index] = "0";
 
                 }
@@ -38,21 +35,13 @@ public class StringCalculator {
         return sum + convertToInteger(sumNumber);
     }
 
-//    public String specificMethod(String[] array, int index) {
-//
-//        if (
-//                array[index].equals("1") &&
-//                        validationStar(array[index - 1])
-//                        && validationStar(array[index - 1]) &&
-//                        !array[index - 1].equals(",") &&
-//                        !array[index + 1].equals(",") && index > 2)
-//        {
-//            return "0";
-//        }
-//        else return "1";
-//    }
+    private boolean isTrue(String[] array, int index) {
+        return array[index].equals("1") &&
+                (validationStar(array[index - 1]) && validationStar(array[index + 1])) &&
+                (!array[index - 1].equals(",") && !array[index + 1].equals(","));
+    }
 
-    // if number 1 between to char so ...
+
     private String[] convertToArray(String array) {
         return array.split("");
     }
@@ -70,4 +59,65 @@ public class StringCalculator {
         String regex = "[^\\w\\s]";
         return Pattern.matches(regex, value);
     }
+
+    private String textCorrection(String sentence) {
+        String[] text = sentence.split("");
+        int index1, index2;
+        String replace = "";
+        if (haveBrokeOpenAndClose(text)) {
+            index1 = sentence.indexOf("[");
+            index2 = sentence.indexOf("]");
+            String find = findWord(index1, index2, sentence);
+            if (!find.isEmpty())
+                return replace(sentence, find);
+
+        } else return sentence;
+
+        return replace;
+    }
+
+    private String replace(String text, String find) {
+        if (text.contains(find) && havaNumber(find)) {
+            return text.replaceAll("[^\\n]" + find, "");
+        } else return text;
+    }
+
+    private boolean havaNumber(String textFind)
+    {
+        boolean containsNumber = false;
+        String[] strings = textFind.split("");
+        for (int index = 0; index < textFind.length(); index++)
+        {
+            if (validation(strings[index]))
+                 containsNumber=true;
+        }
+         return containsNumber;
+    }
+
+    private String findWord(int index1, int index2, String test) {
+        String[] split = test.split("");
+        String word = "";
+        for (int index = index1 + 1; index < index2; index++) {
+            word += split[index];
+        }
+        return word;
+    }
+
+    private boolean haveBrokeOpenAndClose(String[] array) {
+        boolean haveBroke = false;
+        for (int index = 0; index < array.length; index++) {
+            if (array[index].contains("[")) {
+                for (int i = index; i < array.length; i++) {
+                    if (array[i].contains("]")) {
+                        haveBroke = true;
+                        break;
+                    }
+                }
+                break;
+            }
+
+        }
+        return haveBroke;
+    }
+
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -51,20 +51,46 @@ class StringCalculatorTest {
     public void enterNumberWhitCharacter() {
         int add = calculator.add("1\n2,3");
         assertEquals(6, add);
+
         int add1 = calculator.add("//;\n1;2");
         assertEquals(3, add1);
+
+        int add8 = calculator.add("//[e];\n1;2");
+        assertEquals(3, add8);
+
         int add2 = calculator.add("//[*][%]\n1*2%3");
         assertEquals(6, add2);
+
         int add3 = calculator.add("//[***]\n1***2***3");
         assertEquals(6, add3);
+
         int add4 = calculator.add("//[foo][bar]\n1foo2bar3");
         assertEquals(6, add4);
+
+    }
+
+    @Test
+    public void separatorsWithoutExit() {
+        int add = calculator.add("//[e];\n1;2");
+        assertEquals(3, add);
+        int add1 = calculator.add("//[w];\n1;2,5");
+        assertEquals(8, add1);
+        int add2 = calculator.add("//[o];\n1");
+        assertEquals(1, add2);
+        int add3 = calculator.add("//[o][%]\n1*2%3");
+        assertEquals(6, add3);
     }
     @Test
     public void numberOneBetweenCharacters()
     {
         int add = calculator.add("//[1]\n1*1*2*1*3");
         assertEquals(6, add);
+
+        int add1 = calculator.add("//[1_€#@546]\n1*1_€#@546*2*1_€#@546*3");
+        assertEquals(6, add1);
+
+        int add2 = calculator.add("//[1_€#@]\n1*1_€#@*2*1_€#@*3");
+        assertEquals(6, add2);
     }
 
     @Test
